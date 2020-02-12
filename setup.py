0dc6771729b6df9704b07d750e6d475188a6687a
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-02-12 13:26:57 +0100

    Update python wrapper with full functionality

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup
 
-__version__ = '1.0'
+__version__ = '2.0'
 
-setup( name='arrow1', version=__version__, py_modules=['arrow1'] )
+setup(
+    name='arrow1', 
+    version=__version__, 
+    py_modules=['arrow1'],
+    install_requires=[
+        'scipy',
+        'numpy'
+    ]
+)
