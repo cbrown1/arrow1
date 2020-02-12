0819588d71bdf8b596afbb5ec5315af3fe632cda
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-02-12 17:01:13 +0100

    Disable debugging output by default

--- cli.hpp
+++ cli.hpp
@@ -7,12 +7,7 @@ struct Args {
     static const vector<string> PORTS_DEFAULT;
 
     bool show_ports = false;
-    bool debug
-#ifdef NDEBUG
-        = false;
-#else
-        = true;
-#endif
+    bool debug = false;
     bool show_version = false;
     size_t buffer_size = BUFFER_SIZE_DEFAULT;
     vector<string> input_ports = PORTS_DEFAULT; 
