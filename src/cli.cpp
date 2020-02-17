a7a6baa18bf1abbc08d60f690e633415015c3a24
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-02-17 19:06:28 +0100

    Update changed names of CLI params in all places

--- src/cli.cpp
+++ src/cli.cpp
@@ -15,7 +15,7 @@ namespace {
 auto split_ports(const vector<string>& ports) {
     vector<string> res;
     for (auto& port: ports) {
-        boost::tokenizer<boost::char_separator<char>> tok(port, 
+        boost::tokenizer<boost::char_separator<char>> tok(port,
             boost::char_separator<char>(","));
         std::copy(tok.begin(), tok.end(), std::back_inserter(res));
     }
@@ -28,7 +28,7 @@ bool validate(const po::variables_map& vm, Args& args) {
         return true;
     }
     if (args.output_file.empty() && args.input_file.empty()) {
-        std::cerr << ABOUT + 
+        std::cerr << ABOUT +
         "\nNo playback or record file specified. Nothing to do!\n";
         return false;
     }
@@ -55,36 +55,36 @@ Args handle_cli(int argc, char** argv) {
     Args args;
     po::options_description opts("Options");
     opts.add_options()
-        ("help,h", 
+        ("help,h",
             "Print this help message & exit")
-        ("version,v", po::bool_switch(&args.show_version), 
+        ("version,v", po::bool_switch(&args.show_version),
             "Print version info & exit")
-        ("channels,c", po::bool_switch(&args.show_ports), 
+        ("channels,c", po::bool_switch(&args.show_ports),
             "Print available Jack channels & exit")
-        ("debug,d", po::bool_switch(&args.debug), 
+        ("debug,d", po::bool_switch(&args.debug),
             "Allow debugging output")
-        ("buffer,b", po::value(&args.buffer_size), 
+        ("buffer,b", po::value(&args.buffer_size),
             "Jack buffer size in samples")
-        ("in,i", po::value(&args.input_ports), 
+        ("in,i", po::value(&args.input_ports),
             "Jack input (record) channels, specified using a comma-separated list; first item specifies soundfile ch 1, etc")
-        ("out,o", po::value(&args.output_ports), 
+        ("out,o", po::value(&args.output_ports),
             "Jack output (playback) channels, specified using a comma-separated list; first item specifies soundfile ch 1, etc")
-        ("duration,D", po::value(&args.duration_secs), 
+        ("duration,D", po::value(&args.duration_secs),
             "Duration of playback and recording in s; if not set, will be equal to the length of playback file")
-        ("start,s", po::value(&args.start_offset_secs), 
+        ("start,s", po::value(&args.start_offset_secs),
             "Offset to start at when reading audio file, in s")
         ("play-file,p", po::value(&args.input_file), "File path to read playback audio from, in any format supported by libsndfile")
         ("record-file,r", po::value(&args.output_file), "File path to write recorded audio to, in wav format")
     ;
     po::positional_options_description pos;
-    pos.add("input-file", 1).add("output-file", 1);
+    pos.add("record-file", 1).add("play-file", 1);
 
     po::variables_map vm;
     try {
         po::store(po::command_line_parser(argc, argv)
                 .options(opts)
                 .positional(pos)
-                .run(), 
+                .run(),
             vm);
         po::notify(vm);
     } catch (po::error& e) {
